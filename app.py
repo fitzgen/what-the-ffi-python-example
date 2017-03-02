8dca5401d108e5f5462119473e18915759e9dd09
Nick Fitzgerald <fitzgen@gmail.com>
2017-03-01 16:27:05 -0800

    Manage a cache of Levenshtein distances in Python

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
 #!/usr/bin/env python3
 
-from ctypes import byref, cdll, c_size_t
+from ctypes import byref, cdll, c_size_t, POINTER, Structure
 
 lib = cdll.LoadLibrary("target/debug/libpython_example.dylib")
 
-left = b"wutang"
-right = b"mutate"
+class LevDistCache(Structure):
+    pass
 
-distance = c_size_t(0)
-ok = lib.levenshtein_distance(left,
-                              len(left),
-                              right,
-                              len(right),
-                              byref(distance))
+ldc_new = lib.ldc_new
+ldc_new.restype = POINTER(LevDistCache)
 
-if ok != 0:
-    print("Uh oh, something went wrong")
-else:
-    print("The levenshtein distance between {} and {} is {}".format(
-        left, right, distance))
+ldc_delete = lib.ldc_delete
+ldc_delete.restype = None
+
+cache = ldc_new()
+print("Allocated the cache")
+
+for _ in range(10):
+    left = b"wutang"
+    right = b"mutate"
+
+    distance = c_size_t(0)
+    ok = lib.ldc_distance(cache,
+                          left,
+                          len(left),
+                          right,
+                          len(right),
+                          byref(distance))
+
+    if ok != 0:
+        print("Uh oh, something went wrong")
+        break
+    else:
+        print("The Levenshtein distance between {} and {} is {}".format(
+            left, right, distance))
+
+ldc_delete(cache)
+cache = None
+
+print("Deallocated the cache")
--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,64 @@ extern crate libc;
 extern crate strsim;
 
 use libc::{c_char, c_int, size_t};
+use std::collections::HashMap;
+use std::mem;
 use std::panic;
+use std::ptr;
 use std::slice;
 use std::str;
 
+// Ok not to be `#[repr(C)]` because we only expose it behind
+// pointers, which are `#[repr(C)]`.
+pub struct LevDistCache {
+    cache: HashMap<(String, String), usize>,
+}
+
+impl LevDistCache {
+    fn new() -> Box<LevDistCache> {
+        Box::new(LevDistCache {
+            cache: HashMap::new()
+        })
+    }
+
+    fn distance(&mut self, left: &str, right: &str) -> usize {
+        *self.cache
+            .entry((left.into(), right.into()))
+            .or_insert_with(|| strsim::levenshtein(left, right))
+    }
+}
+
 #[no_mangle]
-pub extern fn levenshtein_distance(left: *const c_char,
-                                   left_length: size_t,
-                                   right: *const c_char,
-                                   right_length: size_t,
-                                   result: *mut size_t) -> c_int {
+pub extern fn ldc_new() -> *mut LevDistCache {
     let result = panic::catch_unwind(|| {
-        if left.is_null() || right.is_null() || result.is_null() {
+        let mut cache = LevDistCache::new();
+        let result = {
+            &mut *cache as *mut _
+        };
+        mem::forget(cache);
+        result
+    });
+    match result {
+        Ok(r) => r,
+        Err(_) => ptr::null_mut(),
+    }
+}
+
+#[no_mangle]
+pub extern fn ldc_distance(cache: *mut LevDistCache, left: *const c_char,
+                           left_length: size_t,
+                           right: *const c_char,
+                           right_length: size_t,
+                           result: *mut size_t) -> c_int {
+    let result = panic::catch_unwind(|| {
+        if cache.is_null() || left.is_null() ||
+           right.is_null() || result.is_null() {
             return 1;
         }
 
-        let (left, right) = unsafe {
-            (slice::from_raw_parts(left as *const u8, left_length),
+        let (cache, left, right) = unsafe {
+            (mem::transmute::<_, &mut LevDistCache>(cache),
+             slice::from_raw_parts(left as *const u8, left_length),
              slice::from_raw_parts(right as *const u8, right_length))
         };
 
@@ -32,12 +73,11 @@ pub extern fn levenshtein_distance(left: *const c_char,
             Err(_) => return 1,
         };
 
-        let distance = strsim::levenshtein(left, right);
+        let distance = cache.distance(left, right);
 
         let mut result = unsafe {
             result.as_mut()
                 .expect("null checked at beginning")
-
         };
 
         *result = distance;
@@ -47,4 +87,13 @@ pub extern fn levenshtein_distance(left: *const c_char,
         Ok(r) => r,
         Err(_) => 1,
     }
+}
+
+#[no_mangle]
+pub extern fn ldc_delete(cache: *mut LevDistCache) {
+    let _ = panic::catch_unwind(|| {
+        unsafe {
+            let _: Box<LevDistCache> = mem::transmute(cache);
+        }
+    });
 }
